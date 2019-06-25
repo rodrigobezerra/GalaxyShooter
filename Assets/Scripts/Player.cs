edb6dd9619513a656e9262ebf9ea48ceba2789bd
Rodrigo Bezerra <rodrigo.pinheirob@gmail.com>
2019-06-25 10:33:51 -0400

    Implemented player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 // Monobehavior é a super classe que permite ao Unity adicionar comportamentos a gameObjects do projeto
 public class Player : MonoBehaviour
 {
-
-    public float speed;
+    [SerializeField]
+    private float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Name: " + name);
         transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {      
+        Movements();   
+    }
+
+    private void Movements() {
         // Entrada do eixo horizontal (Edit -> Project Settings -> Input)
         // Negativo: movimentando-se para a esquerda;
         // Zero: objeto parado;
@@ -27,7 +29,34 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         // Time.deltaTime:  é o intervalo de tempo entre um Update e outro
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
-        
         transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        // Y BOUNDARIES 
+        if (transform.position.y > 0) 
+        {
+            transform.position = new Vector3(transform.position.x, 0.0f, 0.0f); 
+        } 
+        else if(transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0.0f); 
+        } 
+        // X BOUNDARIES - WRAPPING MODE
+        if (transform.position.x < -9.5f) 
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0.0f); 
+        } 
+        else if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0.0f);
+        }      
+        
+        // if (transform.position.x < -8.5f) 
+        // {
+        //     transform.position = new Vector3(-8.5f, transform.position.y, 0.0f); 
+        // } 
+        // else if (transform.position.x > 8.5f)
+        // {
+        //     transform.position = new Vector3(8.5f, transform.position.y, 0.0f);
+        // }            
     }
 }

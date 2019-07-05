2b60c4450feb268a6a0bfa1bd0f5863a3dc26118
Rodrigo Bezerra <rodrigo.pinheirob@gmail.com>
2019-07-04 22:09:53 -0400

    Implemented Speed boost Powerup

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -7,7 +7,10 @@ public class Powerup : MonoBehaviour
 
     [SerializeField]
     private float _speed = 2.0f;
-
+    
+    [SerializeField]
+    private int _powerupID; // {0} tripple shoot {1} speed boost {2} shields
+    
     // Update is called once per frame
     void Update()
     {
@@ -24,12 +27,25 @@ public class Powerup : MonoBehaviour
             if (player)
             {
                 // enable tripple shoot
-                player.TrippleShootPowerupOn();                
+                if (_powerupID == 0)
+                    player.TrippleShootPowerupOn();
+
+                // enable speed boost here
+                if (_powerupID == 1)
+                {
+                    player.SpeedBoostPowerupOn();
+                }
+
+                // enable shield poweup
+                if (_powerupID == 2)
+                {
+
+                }
+
             }
 
             // destroy ourself
             Destroy(this.gameObject);
         }
     }
-    
 }
